c7ca825b7abc9590deaada960334b6d324fc0c7b
AzureAD\LO <vasile.n@mejix.com>
2023-06-07 15:21:50 +0300

    add new jenkins step

---
+++ vars/monorepo.groovy
@@ -0,0 +1,13 @@
+/**
+ * The step entry point.
+ */
+def call() {
+    String repositoryName = env.JOB_NAME.split('/')[1]
+    String rootFolderPath = "Generated/$repositoryName"
+
+    println rootFolderPath
+
+    //List<String> jenkinsfilePaths = provisionItems(rootFolderPath, env.GIT_URL)
+    //List<String> multibranchPipelinesToRun = findMultibranchPipelinesToRun(jenkinsfilePaths)
+    //runPipelines(rootFolderPath, multibranchPipelinesToRun)
+}
