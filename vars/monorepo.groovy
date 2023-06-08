13c428f48341a38e30e31c16bf2903d62024b398
AzureAD\LO <vasile.n@mejix.com>
2023-06-08 10:54:11 +0300

    Change methos

--- vars/monorepo.groovy
+++ vars/monorepo.groovy
@@ -1,4 +1,4 @@
-import com.functions.ProvisionItems
+//import com.functions.ProvisionItems
 
 /**
  * The step entry point.
@@ -10,12 +10,31 @@ def call() {
 
     println rootFolderPath
     println env.GIT_URL
-
-    ProvisionItems pi = new ProvisionItems()
-    println "Ajunge aici"
-    List<String> jenkinsfilePaths = pi.provisionItems(rootFolderPath, env.GIT_URL)
+    
+    List<String> jenkinsfilePaths = provisionItems(rootFolderPath, env.GIT_URL)
     
     //List<String> multibranchPipelinesToRun = findMultibranchPipelinesToRun(jenkinsfilePaths)
     //runPipelines(rootFolderPath, multibranchPipelinesToRun)
    } 
 }
+
+
+ List<String> provisionItems(String rootFolderPath, String repositoryURL) {
+    // Find all Jenkinsfiles.
+    List<String> jenkinsfilePaths = findFiles(glob: '**/*/Jenkinsfile').collect { it.path }
+
+    // Provision folder and Multibranch Pipelines.
+    jobDsl(
+            scriptText: libraryResource('multiPipelines.groovy'),
+            additionalParameters: [
+                    jenkinsfilePathsStr: jenkinsfilePaths,
+                    rootFolderStr      : rootFolderPath,
+                    repositoryURL      : env.GIT_URL
+            ],
+            // The following may be set to 'DELETE'. Note that branches will compete to delete and recreate items
+            // unless you only provision items from the default branch.
+            removedJobAction: 'IGNORE'
+    )
+
+    return jenkinsfilePaths
+ }
