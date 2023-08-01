4cca2c16d260c2dae92c7e2e1610f59280b5e5ce
AzureAD\LO <vasile.n@mejix.com>
2023-08-01 16:19:35 +0300

    Increase timeout

--- vars/monorepo.groovy
+++ vars/monorepo.groovy
@@ -122,7 +122,7 @@ def runPipelines(String rootFolderPath, List<String> multibranchPipelinesToRun) 
             // corresponding Pipeline under the Multibranch Pipeline item. We have to wait for Jenkins to process the
             // event so a build can be triggered.
             println pipelineName
-            timeout(time: 5, unit: 'MINUTES') {
+            timeout(time: 15, unit: 'MINUTES') {
                 waitUntil(initialRecurrencePeriod: 1e3) {
                     def pipeline = Jenkins.instance.getItemByFullName(pipelineName)
                     pipeline && !pipeline.isDisabled()
